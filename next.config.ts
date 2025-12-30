2a26c853a8cd49ef2429ac3bb5273bd8fcafc896
yuna park <dbsdk95331@naver.com>
2025-12-30 17:44:26 +0900

    basePath,assetPrefix 삭제

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
   output: "export",
-  basePath: isProd ? "/username.github.io" : "",
-  assetPrefix: isProd ? "/username.github.io/" : "",
   images: {
     unoptimized: true,
   },
