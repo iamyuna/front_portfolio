b31a4d7a9663d6bcf82a4309a540381140a452a5
yuna park <dbsdk95331@naver.com>
2026-02-03 23:33:56 +0900

    모바일 이미지 크기 수정

--- components/MainCard.tsx
+++ components/MainCard.tsx
@@ -13,7 +13,7 @@ export default function MainCard({href, img, label, className = ""}: MainCardPro
         <div className={`relative overflow-hidden group bg-black ${className}`}>
             <Link href={href} className="absolute top-0 left-0 w-full h-full">
                 <div className="w-full h-full transition duration-[600ms] lg:group-hover:scale-110 lg:group-hover:opacity-30">
-                    <img src={img} alt={`${label} 썸네일`} />
+                    <img className="w-full h-full" src={img} alt={`${label} 썸네일`} />
                 </div>
                 <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 overflow-hidden">
                     <p className="text-[1.1rem] text-white translate-y-full transition duration-[600ms] lg:group-hover:translate-y-0">{label}</p>
